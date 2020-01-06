416ed7caede8f81548d0a49edcb3a20bfc9e758f
neilhanyang <neilhanyang@didiglobal.com>
2020-01-06 15:00:17 +0800

    add decode main file

--- build_graph_main.py
+++ build_graph_main.py
@@ -1,18 +1,24 @@
-import sys
 from absl import logging
 from graph import GraphBuilder
 
+def main(input_speller_file, input_chars_file, input_arpa_file,
+        output_disambig_chars_file='characters_disambig.txt',
+        output_words_file='words.txt',
+        output_fst_file='SG.fst'):
+    graph_builder = GraphBuilder()
+    graph_builder.make_graph(
+            input_speller_file,
+            input_chars_file,
+            input_arpa_file,
+            output_disambig_chars_file,
+            output_words_file,
+            output_fst_file)
+
 if __name__ == '__main__':
     logging.set_verbosity(logging.INFO)
-    if len(sys.argv) != 4 and len(sys.argv) != 7:
-        logging.warning(('Usage: python {} input_speller_file input_chars_file input_arpa_file\n'
-            ' output_disambig_chars_file output_words_file output_fst_file'.format(sys.argv[0])))
-        logging.warning('output_disambig_chars_file="characters_disambig.txt"\n'
-                'output_words_file="words.txt"\n'
-                'output_fst_file="SG.fst"')
-    graph_builder = GraphBuilder()
-    if len(sys.argv) == 4:
-        graph_builder.make_graph(sys.argv[1], sys.argv[2], sys.argv[3])
-    if len(sys.argv) == 7:
-        graph_builder.make_graph(sys.argv[1], sys.argv[2], sys.argv[3],
-                sys.argv[4], sys.argv[5], sys.argv[6])
+    main('egs/hkust/graph/speller.txt',
+        'egs/hkust/graph/characters.txt',
+        'egs/hkust/graph/lm_hkust.arpa',
+        'egs/hkust/graph/characters_disambig.txt',
+        'egs/hkust/graph/words.txt',
+        'egs/hkust/graph/SG.fst')
--- decode_wfst_main.py
+++ decode_wfst_main.py
@@ -1,56 +1,76 @@
-import sys
-import json
-import time
-import tensorflow as tf
 from absl import logging
-from athena.main import (
-    build_model_from_jsonfile
-)
-import numpy as np
-from athena.tools.wfst_decoder import WFSTDecoder
+from decoders import WFSTDecoder
 
-logging.set_verbosity(logging.INFO)
-jsonfile = "egs/hkust/mtl_transformer.json"
-p, model, _, checkpointer, dataset_builder = build_model_from_jsonfile(jsonfile, 0)
-checkpointer.restore_from_best()
-dataset_builder = dataset_builder.load_csv(p.test_csv).compute_cmvn_if_necessary(True)
-dataset = dataset_builder.as_dataset(batch_size=1)
-vocab = []
-with open(p.dataset_config['text_config']['model'],'r') as f:
-    for line in f:
-        char = line.strip().split()[0]
-        vocab.append(char)
-words = []
-with open('egs/hkust/build_graph/words.txt','r') as f:
-    for line in f:
-        word = line.strip().split()[0]
-        words.append(word)
+class ToyE2EModel:
+    """Toy E2E Model with necessary interfaces
+    
+    Interfaces:
+        get_encoder_outputs: outputs of encoder for encoder-decoder structure model
+            or just outputs for CTC model;the function should be execute only once
+            for every input utterance;Additionally, the return value could be a tuple
+            wrapping outputs of encoder and other necessary information, for example
+            output mask.
+        get_initial_packed_states: initial information for model inference; the return
+            value could be a tuple wrapping any information.
+        inference_one_step:callback function of E2E model;the function take encoder output
+            current label input and corresponding inner information to inference scores for
+            all the output labels; Additionally, the function should return the new inner
+            information for next inference step.
+    """
+    def __init__(self):
+        """Read  scores from file obtained in advance for this toy model"""
+        self.step = 0
+        self.scores_per_step = []
+        batch = []
+        with open('egs/hkust/scores.txt', 'r') as f:
+            for line in f:
+                items = line.strip().split()
+                if len(items) == 4:
+                    if batch != []:
+                        self.scores_per_step.append(batch)
+                    batch = []
+                else:
+                    items = [float(item) for item in items]
+                    batch.append(items)
+        self.scores_per_step.append(batch)
 
-decoder = WFSTDecoder("egs/hkust/build_graph/SG.fst")
-logging.info("load WFST graph successfully")
-trans_file = open("egs/hkust/trans.txt",'w')
-ground_truth_file = open("egs/hkust/ground_truth.txt",'w')
+    def get_encoder_outputs(self, input_feats):
+        """return fake encoder outputs"""
+        return [0.0 for _ in range(100)]
 
-total_feats = 0
-total_time = 0.0
-for index, sample in enumerate(dataset):
-    sample = model.prepare_samples(sample)
-    total_feats += sample['input_length'].numpy()[0]
-    ground_truth = sample['output'][0]
-    ground_truth = ' '.join([vocab[int(idx)] for idx in ground_truth])
-    enc_outputs = model.model.decode(sample, None, return_encoder=True)
+    def get_initial_packed_states(self):
+        """return fake initial states"""
+        return (0,)
 
-    begin = time.time()
-    decoder.decode(enc_outputs, (0,), model.model.inference_one_step)
-    trans = decoder.get_best_path()
-    total_time += time.time()-begin
-    trans = ' '.join([words[int(idx)] for idx in trans])
-    logging.info("predictions: {} labels: {}".format(trans, ground_truth))
-    trans_file.write("{}\n".format(trans))
-    trans_file.flush()
-    ground_truth_file.write("{}\n".format(ground_truth))
-    ground_truth_file.flush()
-logging.info("decoding total time is {}\n".format(total_time))
-logging.info("total feat num is {}\n".format(total_feats))
-trans_file.close()   
-ground_truth_file.close()
+    def inference_one_step(self, encoder_outputs, label_input, packed_inner_states):
+        """model inference one step in batch
+
+        Args:
+            encoder_outputs: encoder outputs, shape=...
+            label_input: current input labels, shape=[batch, label]
+            packed_inner_states: inner states for every input label, shape=[batch, states]
+        """
+        batch_scores = self.scores_per_step[self.step]
+        batch = len(batch_scores)
+        self.step += 1
+        return batch_scores, [(self.step,) for _ in range(batch)]
+
+
+if __name__ == '__main__':
+
+    logging.set_verbosity(logging.INFO)
+    e2e_model = ToyE2EModel()
+    input_feats = [0.0 for _ in range(100)]
+    initial_packed_states = e2e_model.get_initial_packed_states()
+    enc_outputs = e2e_model.get_encoder_outputs(input_feats)
+    label_input = 3650
+    words_table = {}
+    with open('egs/hkust/graph/words.txt', 'r') as f:
+        for line in f:
+            word, idx = line.strip().split()
+            words_table[int(idx)] = word
+    decoder = WFSTDecoder('egs/hkust/graph/SG.fst')
+    decoder.decode(enc_outputs, initial_packed_states, e2e_model.inference_one_step)
+    trans_idx = decoder.get_best_path()
+    trans = ' '.join([words_table[int(idx)] for idx in trans_idx])
+    logging.info("predictions: {}".format(trans))
