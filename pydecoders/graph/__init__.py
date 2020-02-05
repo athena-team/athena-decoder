ad29e219c4740d92bff00263c43a4e33dad1f44f
neilhanyang <neilhanyang@didiglobal.com>
2020-02-05 15:04:20 +0800

    token fst builder for CTC

--- pydecoders/graph/__init__.py
+++ pydecoders/graph/__init__.py
@@ -1,7 +1,9 @@
 from . import grammar_builder
 from . import speller_builder
 from . import graph_builder
+from . import token_builder
 from .grammar_builder import GrammarBuilder
 from .speller_builder import SpellerBuilder
 from .graph_builder import GraphBuilder
+from .token_builder import TokenBuilder
 
