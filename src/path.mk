d3097579a6297c48205fcf0731423942e9ba8681
neilhanyang <neilhanyang@didiglobal.com>
2020-09-03 17:25:04 +0800

    update Makefile

--- src/path.mk
+++ src/path.mk
@@ -1,9 +1,12 @@
 
-TF_DIR := /nfs/cold_project/yuanlong/tensorflow_env/tensorflow_1.14
-TF_INC_DIR := $(TF_DIR)/bazel-tensorflow_1.14
-TF_LIB_DIR := $(TF_DIR)/bazel-bin/tensorflow
-EIGEN := /nfs/cold_project/yuanlong/tensorflow_env/eigen-eigen-323c052e1731
-ABSL := /nfs/cold_project/yuanlong/tensorflow_env/abseil-cpp
-
+TENSORFLOW := ./tools/tensorflow
+EXTRA_CXXFLAGS = -Wno-sign-compare \
+				 -I$(TENSORFLOW)/bazel-tensorflow/external/protobuf_archive/src \
+				 -I$(TENSORFLOW)/bazel-genfiles -I$(TENSORFLOW) \
+				 -I$(TENSORFLOW)/tensorflow/contrib/makefile/downloads/eigen \
+				 -I$(TENSORFLOW)/tensorflow/contrib/makefile/downloads/nsync/public \
+				 -I$(TENSORFLOW)/tensorflow/contrib/makefile/downloads/protobuf/src \
+				 -I${TENSORFLOW}/tensorflow/contrib/makefile/downloads/absl
+TF_LIB_DIR := $(TENSORFLOW)/bazel-bin/tensorflow
 
 
